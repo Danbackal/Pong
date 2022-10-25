9716b0f4c4447284d5f579fa4e201370a36231c2
Daniel Backal <danbackal@gmail.com>
2022-10-25 16:03:00 -0700

    game board setup

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,28 +1,18 @@
 #include "Platform/Platform.hpp"
+#include "Game.cpp"
 
 int main()
 {
 	util::Platform platform;
-
-#if defined(_DEBUG)
-	std::cout << "Hello World!" << std::endl;
-#endif
-
 	sf::RenderWindow window;
 	// in Windows at least, this must be called before creating the window
 	float screenScalingFactor = platform.getScreenScalingFactor(window.getSystemHandle());
 	// Use the screenScalingFactor
-	window.create(sf::VideoMode(200.0f * screenScalingFactor, 200.0f * screenScalingFactor), "SFML works!");
+	window.create(sf::VideoMode(1024.0f * screenScalingFactor, 563.2f * screenScalingFactor), "Pong");
 	platform.setIcon(window.getSystemHandle());
 
-	sf::CircleShape shape(window.getSize().x / 2);
-	shape.setFillColor(sf::Color::White);
-
-	sf::Texture shapeTexture;
-	shapeTexture.loadFromFile("content/sfml.png");
-	shape.setTexture(&shapeTexture);
-
 	sf::Event event;
+	Game game(&window);
 
 	while (window.isOpen())
 	{
@@ -31,10 +21,7 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
+		game.DisplayGame();
 	}
 
 	return 0;
